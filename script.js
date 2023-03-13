5614eab800734f1d27fb673f2693237151f1db85
herakle-dev <ferrigno.ivan98@gmail.com>
2023-03-13 18:15:47 +0100

    daily push

--- script.js
+++ script.js
@@ -1,147 +1,142 @@
-const searchParameter = document.getElementById("searchParameter");
-const searchValue = document.getElementById("searchValue");
-const buttonSearch = document.getElementById('search');
-const lista = document.getElementById('lista');
-let counter = 0;
-function createElement(tag, className, id) {
-  counter++;
-  const element = document.createElement(tag);
-  element.className = className;
-  element.id = `${id}${counter}`;
-  return element;
-}
-function getBooks(searchValue, searchParameter) {
-  let apiUrl;
-  if (searchParameter === "subject") {
-    searchValue = searchValue.trim().toLowerCase();
-    if (searchValue.indexOf(' ') !== -1) {
-      let toast = createElement('div', 'toast error show', 'toast');
-      let message = document.createTextNode('La ricerca per categoria accetta una sola parola');
-      toast.appendChild(message);
-      lista.appendChild(toast);
-      return;
+    const searchParameter = document.getElementById("searchParameter");
+    const searchValue = document.getElementById("searchValue");
+    const buttonSearch = document.getElementById('search');
+    const lista = document.getElementById('lista');
+    let counter = 0;
+    function createElement(tag, className, id) {
+      counter++;
+      const element = document.createElement(tag);
+      element.className = className;
+      element.id = `${id}${counter}`;
+      return element;
     }
-    apiUrl = ` https://openlibrary.org/subjects/${searchValue}.json?limit=50`;
-  } else if (searchParameter === "name") {
-    apiUrl = `https://openlibrary.org/search.json?title=${searchValue}&limit=50`;
-  } else if (searchParameter === "author") {
-    apiUrl = `https://openlibrary.org/search.json?author=${searchValue}&limit=50`;
-  }
-  axios.get(apiUrl)
-    .then(response => {
-
-      const books = searchParameter === "subject" ? response.data.works : response.data.docs;
-
-      if (books.length <= 0) {
-        let toast = createElement('div', 'toast error show', 'toast')
-        let message = document.createTextNode('La ricerca non ha prodotto nessun risultato');
-        toast.appendChild(message);
-        lista.appendChild(toast);
-        setTimeout(() => {
-          lista.removeChild(toast);
-        }, 5000);
+    function getBooks(searchValue, searchParameter) {
+      let apiUrl;
+      if (searchParameter === "subject") {
+        searchValue = searchValue.trim().toLowerCase();
+        if (searchValue.indexOf(' ') !== -1) {
+          let toast = createElement('div', 'toast error show', 'toast');
+          let message = document.createTextNode('La ricerca per categoria accetta una sola parola');
+          toast.appendChild(message);
+          lista.appendChild(toast);
+          return;
+        }
+        apiUrl = `https://openlibrary.org/subjects/${searchValue}.json?limit=50`;
+      } else if (searchParameter === "name") {
+        apiUrl = `https://openlibrary.org/search.json?title=${searchValue}&limit=50`;
+      } else if (searchParameter === "author") {
+        apiUrl = `https://openlibrary.org/search.json?author=${searchValue}&limit=50`;
       }
-      for (let i = 0; i < books.length; i++) {
-        const book = books[i];
-        console.log(book)
-        let bookTitle = book.title;
-        const listElement = createElement(`li`, `col-lg-3 listElement`, `book`);
-        bookTitle = createElement('a', `bookTitle col-lg-12`, `bookTitle`);
-        bookTitle.setAttribute('href', `https://openlibrary.org/${book.key}`)
-        bookTitle.setAttribute('target', '_blank')
-        let cover = `https://covers.openlibrary.org/b/id/${searchParameter === "subject" ? book.cover_id : book.cover_i}.jpg`;
-        const imgCover = createElement('img', `bookCover img col-lg-8`, `bookCover`)
-        imgCover.setAttribute("src", cover);
-        imgCover.setAttribute("alt", "testo alternativo");
-        const descriptionButton = createElement('button', 'descriptionButton btn btn-info  col-lg-12', `btnDescription${counter}`)
-        descriptionButton.innerHTML = 'Mostra descrizione'
-        listElement.appendChild(bookTitle);
-        bookTitle.innerHTML += book.title;
-        lista.appendChild(listElement);
-        let printAuthorsName = createElement('a', 'authorsPara col-lg-12', 'authors');
-        if (searchParameter === 'subject') {
-          authorsArray = book.authors[0].name;
-          authorKey = book.authors[0].key;
-          authorKeyLink = ` https://openlibrary.org/${authorKey}`;
-        } else {
-          authorsArray = book.author_name;
-          authorKey = book.author_key
-          authorKeyLink = `https://openlibrary.org/authors/${authorKey}`
-        }
-        printAuthorsName.setAttribute('href', `${authorKeyLink}`);
-        printAuthorsName.setAttribute('target', '_blank');
-        if (!listElement.printAuthorsName) {
-          listElement.printAuthorsName = true;
-          printAuthorsName.innerHTML = authorsArray;
-          listElement.appendChild(printAuthorsName);
-        }
-        descriptionButton.addEventListener('click', () => {
-          toggleDescription(listElement, book);
+      axios.get(apiUrl)
+        .then(response => {
+          const books = searchParameter === "subject" ? response.data.works : response.data.docs;
+          if (books.length <= 0) {
+            let toast = createElement('div', 'toast error show', 'toast')
+            let message = document.createTextNode('La ricerca non ha prodotto nessun risultato');
+            toast.appendChild(message);
+            lista.appendChild(toast);
+            setTimeout(() => {
+              lista.removeChild(toast);
+            }, 5000);
+          }
+          for (let i = 0; i < books.length; i++) {
+            const book = books[i];
+            
+            let bookTitle = book.title;
+            const listElement = createElement(`li`, `col-lg-3 listElement`, `book`);
+            bookTitle = createElement('a', `bookTitle col-lg-12`, `bookTitle`);
+            bookTitle.setAttribute('href', `https://openlibrary.org/${book.key}`)
+            bookTitle.setAttribute('target', '_blank')
+            let cover = `https://covers.openlibrary.org/b/id/${searchParameter === "subject" ? book.cover_id : book.cover_i}.jpg`;
+            const imgCover = createElement('img', `bookCover img col-lg-8`, `bookCover`)
+            imgCover.setAttribute("src", cover);
+            imgCover.setAttribute("alt", "testo alternativo");
+            const descriptionButton = createElement('button', 'descriptionButton btn btn-info  col-lg-12', `btnDescription${counter}`)
+            descriptionButton.innerHTML = 'Mostra descrizione'
+            listElement.appendChild(bookTitle);
+            bookTitle.innerHTML += book.title;
+            lista.appendChild(listElement);
+            let printAuthorsName = createElement('a', 'authorsPara col-lg-12', 'authors');
+            if (searchParameter === 'subject') {
+              authorsArray = book.authors[0].name;
+              authorKey = book.authors[0].key;
+              authorKeyLink = ` https://openlibrary.org/${authorKey}`;
+            } else {
+              authorsArray = book.author_name;
+              authorKey = book.author_key
+              authorKeyLink = `https://openlibrary.org/authors/${authorKey}`
+            }
+            printAuthorsName.setAttribute('href', `${authorKeyLink}`);
+            printAuthorsName.setAttribute('target', '_blank');
+            if (!listElement.printAuthorsName) {
+              listElement.printAuthorsName = true;
+              printAuthorsName.innerHTML = authorsArray;
+              listElement.appendChild(printAuthorsName);
+            }
+            descriptionButton.addEventListener('click', () => {
+              toggleDescription(listElement, book);
+            })
+            listElement.appendChild(imgCover);
+            listElement.appendChild(descriptionButton);
+          }
         })
-        listElement.appendChild(imgCover);
-        listElement.appendChild(descriptionButton);
-      }
-    })
-    .catch(error => {
-      console.log(` ${error}`);
-    });
-}
-function recuperaDescrizione(work, listElement) {
-  const key = work.key;
-  console.log(`Questo è il link per le api dove si trova la descrizione: https://openlibrary.org/${key}.json`);
-  axios.get(`https://openlibrary.org${key}.json`)
-    .then(response => {
-      let descrizione = response.data.description;
-      let descriptionPara = createElement('p', 'col-lg-12 descriptionPara', 'para')
-      if (!listElement.descriptionPara) {
-        if (!descrizione) {
-          descrizione = 'Nessuna descrizione disponibile.';
-          descriptionPara.innerHTML += descrizione
-          listElement.appendChild(descriptionPara)
-        }
-        else if (typeof descrizione === "object") {
-          descriptionPara.innerHTML = descrizione.value
-          listElement.appendChild(descriptionPara)
-        } else {
-          descriptionPara.innerHTML = descrizione
-          listElement.appendChild(descriptionPara)
-        }
-      }
-      listElement.descriptionVisible = true;
-      listElement.descriptionPara = true;
-
-    })
-    .catch(error => {
-      console.log(`Errore durante la richiesta della descrizione: ${error}`);
-    });
-}
-function toggleDescription(listElement, book) {
-  const descriptionPara = listElement.querySelector('.descriptionPara');
-  const descriptionButton = listElement.querySelector('.descriptionButton')
-  if (listElement.descriptionVisible) {
-    // Rimuovi la descrizione
-    if (descriptionPara) {
-      descriptionPara.remove();
-      listElement.descriptionPara = false;
-      descriptionButton.innerHTML = ' Mostra descrizione'
+        .catch(error => {
+          console.log(` ${error}`);
+        });
     }
-    listElement.descriptionVisible = false;
-  } else {
-    if (!descriptionPara) {
-      // La descrizione è già presente
-      recuperaDescrizione(book, listElement);
-      listElement.descriptionVisible = true;
-      descriptionButton.innerHTML = ' Nascondi descrizione'
+    function recuperaDescrizione(work, listElement) {
+      const key = work.key;
+      console.log(`Questo è il link per le api dove si trova la descrizione: https://openlibrary.org/${key}.json`);
+      axios.get(`https://openlibrary.org${key}.json`)
+        .then(response => {
+          let descrizione = response.data.description;
+          let descriptionPara = createElement('p', 'col-lg-12 descriptionPara', 'para')
+          if (!listElement.descriptionPara) {
+            if (!descrizione) {
+              descrizione = 'Nessuna descrizione disponibile.';
+              descriptionPara.innerHTML += descrizione
+              listElement.appendChild(descriptionPara)
+            }
+            else if (typeof descrizione === "object") {
+              descriptionPara.innerHTML = descrizione.value
+              listElement.appendChild(descriptionPara)
+            } else {
+              descriptionPara.innerHTML = descrizione
+              listElement.appendChild(descriptionPara)
+            }
+          }
+          listElement.descriptionVisible = true;
+          listElement.descriptionPara = true;
 
+        })
+        .catch(error => {
+          console.log(`Errore durante la richiesta della descrizione: ${error}`);
+        });
     }
+    function toggleDescription(listElement, book) {
+      const descriptionPara = listElement.querySelector('.descriptionPara');
+      const descriptionButton = listElement.querySelector('.descriptionButton')
+      if (listElement.descriptionVisible) {
+        // Rimuovi la descrizione
+        if (descriptionPara) {
+          descriptionPara.remove();
+          listElement.descriptionPara = false;
+          descriptionButton.innerHTML = ' Mostra descrizione'
+        }
+        listElement.descriptionVisible = false;
+      } else {
+        if (!descriptionPara) {
+          // La descrizione è già presente
+          recuperaDescrizione(book, listElement);
+          listElement.descriptionVisible = true;
+          descriptionButton.innerHTML = ' Nascondi descrizione'
 
-  }
-}
+        }
 
-
-buttonSearch.addEventListener('click', (e) => {
-  e.preventDefault();
-  lista.innerHTML = '';
-  getBooks(searchValue.value, searchParameter.value)
-
-})
+      }
+    }
+    buttonSearch.addEventListener('click', (e) => {
+      e.preventDefault();
+      lista.innerHTML = '';
+      getBooks(searchValue.value, searchParameter.value)
+    })
